7cfbb408ac1b2827a73eb2c1fee239c4c082073d
Rafael Brunini Pereira <bruninirafael@gmail.com>
2024-06-16 18:49:03 -0300

    Configs de provisionamento e subida de maquina

---
+++ ansible-lab/db01/provision.sh
@@ -0,0 +1,4 @@
+#/bin/sh
+cat << EOT >> /home/vagrant/.ssh/authorized_keys
+ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAABAQC/2v693HWtV1DxTNEfQczIshY2Oehv9HscdN2EOTgbqoB0MFN1zM0Dm9vU2rZIruYUlzOIoWmgkxycCG2SxJZAQTONctxNgl3N8ZUIS4WSW92/59yOEUgMkLoHljSiak63wMcJya/H3SS0HuXS2D5bmjNEQyTp5FQKZqIntaraD/M9ZylK2hQ0kDTkCQ3kVhzao/I09MHUu+0E+2XQTvK4F0GzX0YZ9jijq3pVhdcWd1l0qkPUKJomj3hfxQ6TwNcwmiZtMFvjg+8BNHIRBfD+9jIEw7firVYaYZNo759aYdQDtqr4bW7hXGe5UZRV74RtGOFszYAlf+qrQtbxQOZr vagrant@control-node
+EOT
